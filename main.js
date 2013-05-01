611116920c4f6d1e69ae3daa199a6961679e4828
genzouw <genzouw@gmail.com>
2013-05-02 01:11:11 +0900

    放物線ロジック追加

--- main.js
+++ main.js
@@ -1,78 +1,87 @@
-$(document).ready(function(){
-    //    $("#block").animate(
-    //        {
-    //            left:"+50px",
-    //        }
-    //        );
+$(() => {
+  $("#one_fall").click(() => {
+    $("#block").animate(
+      {
+        top: "500px",
+      },
+      "normal",
+      "easeInCirc",
+    )
+  })
 
-    jQuery.extend(
-        jQuery.easing,
+  $("#any_fall").click(() => {
+    $("<div />").addClass('block').appendTo($("#sandbox")).css({
+      left: Math.random() * 500,
+    })
+      .animate(
         {
-            easeInExpo:function (x, t, b, c, d) {
-                            return t*t;
-//                            return 9.8*t*t/2;
-                        },
-        });
+          top: "500px",
+        },
+        "normal",
+        "easeInCirc",
+      )
+  })
 
-    $("#one_fall").click(function() {
-        $("#block").animate(
-            {
-                top:"500px",
-            },
-            "normal",
-            "easeInCirc"
-            );
-    });
+  $("#any_up").click(() => {
+    $("<div />").addClass('block').appendTo($("#sandbox")).css({
+      top: "500px",
+      left: Math.random() * 500,
+    })
+      .animate(
+        {
+          top: "0px",
+        },
+        "normal",
+        "easeOutCirc",
+      )
+      .animate(
+        {
+          top: "500px",
+        },
+        "normal",
+        "easeInCirc",
+      )
+  })
 
-    $("#any_fall").click(function() {
-        $("<div />").addClass('block').appendTo($("#sandbox")).css({
-            'left':Math.random() * 500,
-        }).animate(
-            {
-                top:"500px",
-            },
-            "normal",
-            "easeInCirc"
-        );
-    });
+  $("#any_arrow").click(() => {
+    $("<div />").addClass('block').appendTo($("#sandbox")).css({
+      top: "500px",
+      left: '0px',
+    })
+      .animate(
+        {
+          top: "0px",
+        },
+        {
+          duration: 400,
+          easing: "easeOutCirc",
+          queue: true,
+        },
+      )
+      .animate(
+        {
+          top: "500px",
+        },
+        {
+          duration: 400,
+          easing: "easeInCirc",
+          queue: true,
+        },
+      )
+      .animate(
+        {
+          left: "500px",
+        },
+        {
+          duration: 800,
+          easing: "linear",
+          queue: false,
+        },
+      )
+  })
 
-    $("#any_up").click(function() {
-        $("<div />").addClass('block').appendTo($("#sandbox")).css({
-            top:"500px",
-            'left':Math.random() * 500,
-        }).animate(
-            {
-                top:"0px",
-            },
-            "normal",
-            "easeOutCirc"
-        ).animate(
-            {
-                top:"500px",
-            },
-            "normal",
-            "easeInCirc"
-        );
-    });
-
-    $("#any_arrow").click(function() {
-        $("<div />").addClass('block').appendTo($("#sandbox")).css({
-            top:"500px",
-            'left':'0px'
-        }).animate(
-            {
-                top:"0px",
-                left:"300px",
-            },
-            "normal",
-            "easeOutCirc"
-        ).animate(
-            {
-                top:"500px",
-                left:"600px",
-            },
-            "normal",
-            "easeInCirc"
-        );
-    });
-});
+  $("#any_two").click(() => {
+    $("#any_up").trigger('click')
+    $("#any_arrow").trigger('click')
+  })
+})
